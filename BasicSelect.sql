c5d0a4bf0586efc87dd49d65792b9d4df3f7401e
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-03-14 12:14:32 -0400

    Update BasicSelect.sql

--- BasicSelect.sql
+++ BasicSelect.sql
@@ -195,5 +195,20 @@ WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID;
 
 
+/*
+Employee Names
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+*/
+
+SELECT name FROM employee
+ORDER BY name;
 
 
+
+/*
+Employee Salaries
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+*/
+
+SELECT name FROM employee
+WHERE salary > 2000 AND months <10;
