5f492cede43069acfa4d1765ac16173a5cd3e51b
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-03-14 11:04:20 -0400

    Update BasicSelect.sql

--- BasicSelect.sql
+++ BasicSelect.sql
@@ -1,7 +1,7 @@
 /*
 Revising the Select Query I
 Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
-/*
+*/
 
 SELECT * FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
