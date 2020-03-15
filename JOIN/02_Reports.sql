bd6b5aed37f92a6f6251e5feea852ceb4a824673
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-03-15 11:55:15 -0400

    Create 02_Reports.sql

---
+++ JOIN/02_Reports.sql
@@ -0,0 +1,30 @@
+/*
+You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+
+Students
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | integer  |
+| Name        | String   |
+| Marks       | integer  |
++-------------+----------+
+
+
+Grades
++-------------+----------+----------+
+| Grade       | Min_Mark | Max_Mark |
++-------------+----------+----------+
+| 1           | 0        | 9        |
+| 2           | 10       | 19       |
+| 3           | 20       | 29       |
+| 4           | 30       | 39       |
+| 5           | 40       | 49       |
+| 6           | 50       | 59       |
+| 7           | 60       | 69       |
+| 8           | 70       | 79       |
+| 9           | 80       | 89       |
+| 10          | 90       | 100      |
++-------------+----------+----------+
+
+*/
