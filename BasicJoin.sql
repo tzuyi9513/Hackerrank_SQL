2c9e8edff27e46ecbf5e247de72ae23695c33fc4
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-03-15 10:54:42 -0400

    Update BasicJoin.sql

--- BasicJoin.sql
+++ BasicJoin.sql
@@ -1,4 +1,41 @@
 /*
+Given two tables, City and Country whose description are given below. Print the name of all continents (key: Country.Continent) along with the average City population rounded down to nearest integer.
+City
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | Number   |
+| Name        | char(17) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | Number   |
++-------------+----------+
+Country
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | char(3)     |
+| Name           | char(44)    |
+| Continent      | char(13)    |
+| Region         | char(25)    |
+| SurfaceArea    | Number      |
+| IndepYear      | char(5)     |
+| Population     | Number      |
+| LifeExpectancy | char(4)     |
+| GNP            | Number      |
+| GNPOld         | char(9)     |
+| LocalName      | char(44)    |
+| GovernmentForm | char(44)    |
+| HeadOfState    | char(32)    |
+| Capital        | char(4)     |
+| Code2          | char(2)     |
++----------------+-------------+
+PS #1: City.CountryCode and Country.Code is same key. 
+PS #2: Continent without cities should not be included in output.
+*/
+
+
+/*
 Asian Population
 Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
@@ -10,3 +47,27 @@ JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
 WHERE COUNTRY.CONTINENT LIKE 'Asia';
 
 
+
+/*
+African Cities
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT CITY.NAME
+FROM CITY
+JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.CONTINENT LIKE 'Africa';
+
+
+
+/*
+Average Population of Each Continent
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.POPULATION)) AS 'AVERAGE'
+FROM CITY
+JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent;
