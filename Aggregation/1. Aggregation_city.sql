4dd7025cb3f3fe7d3e92f288e71765596ab0c094
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-03-15 16:17:16 -0400

    Update 1. Aggregation_city.sql

--- Aggregation/1. Aggregation_city.sql
+++ Aggregation/1. Aggregation_city.sql
@@ -1,4 +1,3 @@
-  
 /*
          City
 +-------------+----------+
@@ -21,3 +20,49 @@ SELECT COUNT(*)
 FROM city
 WHERE population >100000;
 
+
+
+/*
+Query the total population of all cities in CITY where District is California.
+*/
+
+SELECT SUM(population)
+FROM city
+WHERE district = 'California';
+
+
+
+/*
+Query the average population of all cities in CITY where District is California.
+*/
+
+SELECT AVG(population)
+FROM city
+WHERE district = 'California';
+
+
+
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+
+SELECT ROUND(AVG(population),0)
+FROM city;
+
+
+
+/*
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+*/
+
+SELECT SUM(population)
+FROM city
+WHERE countrycode = 'JPN';
+
+
+/*
+Query the difference between the maximum and minimum populations in CITY.
+*/
+
+SELECT MAX(population)-MIN(population) AS difference
+FROM city;
